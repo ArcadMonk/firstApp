bcde7696ceed4e11d76f54feb10fe11535aa65db
Baptiste Duchateau <baptiste.duchateau@gmail.com>
2020-02-17 20:25:01 +0100

    list position/création OK

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.myfirstandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+
+private val TAG = "MyActivity"
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button_id.setOnClickListener { edited_text_id.text = edited_text_id.text.toString() + "E"}
+        val names = listOf("Antoine", "Baptiste ", "Emilie", "Jerome", "Gus")
+        names.sortedBy { it.length }
+        Log.d( TAG , names.toString())
+        namesList_id.text = names.toString()
     }
 }
